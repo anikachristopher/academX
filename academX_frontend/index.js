cca19eee600a89babb50546bb7eeb3ece005618f
Anika Christopher <anika.mchristopher@gmail.com>
2021-04-29 23:55:47 -0400

    setup intended app functionality

---
+++ academX_frontend/index.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {});
+
+//global functions
+
+//function for getting all schedules and children
+//function for creating a new child
+//function for creating a schedule
+//function to show all schedules
+//function to add both child and schedules together
+//function to delete and a button
+//function to add new records
+//function to clear the modal
+
+
+
+
+
+function getAllSchedulesWithChild(){}
+function deleteEntry(){}
+function deleteButton(){}
+function createChild(){}
+function displaySchedules(){}
+function addNewRecords(){}
+function createSchedule(){}
+function clearModal(){}
+function createPost(){}
