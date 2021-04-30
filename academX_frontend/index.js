3b3ba6c18791d8cb40d43f7b149e5cecb6ae5621
Anika Christopher <anika.mchristopher@gmail.com>
2021-04-30 04:52:23 -0400

    create function to show all schedules

--- academX_frontend/index.js
+++ academX_frontend/index.js
@@ -1,4 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {});
+let master;
+let masterCount;
+let masterCountMaxId;
+let pointer = 0;
+const modals = document.getElementById("myModal");
+const scheduleEntry = document.getElementById("schedules-container");
+
 
 const BASE_URL = "http://127.0.0.1:3000";
 
@@ -22,18 +28,66 @@ function getAllSchedulesWithChild(){
     .then((resp) => {
         return resp.json();
     })
-    // .then((schedules) => {displaySchedules(schedules);
-    // })
-    // .catch((err) => {
-    //     console.log("Figure out error message:", err);
-    // });
+    .then((schedules) => {displaySchedules(schedules);
+    })
+    .catch((err) => {
+        console.log("Figure out error message:", err);
+    });
 
 }
+
+
+function displayMasterSchedules(masterSchedule){
+    let id;
+  for (let i = 0; i < masterCount; i++){
+      id = masterSchedule[i].scheduleId;
+      let child_id = masterSchedule[i].id;
+      let firstName = masterSchedule[i].firstName;
+      let lastName = masterSchedule[i].lastName;
+      let age = masterSchedule[i].age;
+      let grade = masterSchedule[i].grade;
+      buildChild(id);
+      let newPost = document.getElementById("schedules-container");
+      newPost.insertAdjacentHTML(
+        "beforeend",
+        `<br>
+        <div class="ui segment">
+            <h4 class="ui horizontal divider header">
+            <i class="child icon"></i>
+            Child
+            </h4>
+            <h5>Id: ${child_id}</h5>
+            <h5>First Name: ${firstName}</h5>
+            <h5>Last Name: ${lastName}</h5>
+            <h5>Age: ${age}</h5>
+            <h5>Grade: ${grade}</h5>
+            <h4 class="ui horizontal divider header">
+             <i class="building icon"></i>
+             Schedule
+             </h4>
+             <div id="children-container${id}"></div>
+          <br />
+          <button id=${id} class="delBtn" type="button">Discard</button>
+          <br/ >
+        <br />
+      </div>`
+    );
+  }
+
+
+}
+
+
+
+
+
 function deleteEntry(){}
 function deleteButton(){}
-function createChild(){}
-function displaySchedules(){}
+function buildChild(){}
+
 function addNewRecords(){}
 function createSchedule(){}
 function clearModal(){}
 function createPost(){}
+
+document.addEventListener("DOMContentLoaded", () => {});
