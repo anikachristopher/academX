553df2f5ca3d887f9192b27fc881aa6ad98132b0
Anika Christopher <anika.mchristopher@gmail.com>
2021-04-29 23:05:19 -0400

    seeded database

--- academX_backend/db/seeds.rb
+++ academX_backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Child.create([{first_name: "Malik", last_name: "Morris", age: "9", grade: "4"}])
+
+Schedule.create([
+    {weekday: "Monday", date: "4/26/2021", subject: "Math", content: "Completed chapter 1 of Go Math", child_id: 1}
+])
