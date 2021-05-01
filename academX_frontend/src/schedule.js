6c000d322808e1dfdec75edf25b95d576e52388d
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-01 19:03:43 -0400

    added code to open the modal

--- academX_frontend/src/schedule.js
+++ academX_frontend/src/schedule.js
@@ -1,3 +1,34 @@
 class Schedule{
     //constructor function
 }
+
+
+
+//MODAL CODE
+
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// const entryForm = document.querySelector("#entryForm");
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
