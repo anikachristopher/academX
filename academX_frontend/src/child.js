542afadeda9da5597e0878b602acfc58c137bf23
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-21 10:23:40 -0400

    built constructor in child class

--- academX_frontend/src/child.js
+++ academX_frontend/src/child.js
@@ -1,3 +1,13 @@
 class Child{
     //build constructor function
+    //Properties
+  ChildrenRegister = [];
+  allChildURL = "";
+  listElement = "";
+
+  //build constructor function
+  constructor(listElement, BASE_URL) {
+    this.listElement = listElement;
+    this.allChildURL = `${BASE_URL}/children`;
+  }
 }
