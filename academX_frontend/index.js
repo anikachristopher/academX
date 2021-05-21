63906ebf27e71958b30be91b87dc7fe5648bae2b
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-21 10:38:05 -0400

    refactored function to get all schedules

--- academX_frontend/index.js
+++ academX_frontend/index.js
@@ -7,6 +7,18 @@ const BASE_URL = "http://127.0.0.1:3000";
 const childClass = new Child("childList", BASE_URL);
 const scheduleClass = new Schedule(BASE_URL);
 
+//global functions
+
+//function for getting all schedules and children
+//function for creating a new child
+//function for creating a schedule
+//function to show all schedules
+//function to add both child and schedules together
+//function to delete and a button
+//function to add new records
+//function to clear the modal
+// basic delete functionality 
+
 
 //gets data for all schedules upon start up and send them to display...
 function getAllSchedulesWithChild() {
@@ -35,34 +47,79 @@ function getAllSchedulesWithChild() {
       });
   }
 
-//global functions
+function deleteButton(){
+    let btns = document.querySelectorAll(".delBtn");
+    for (let i = 0; i < btns.length; i++){
+        btns[i].addEventListener("click", deleteEntry);
+  }
 
-//function for getting all schedules and children
-//function for creating a new child
-//function for creating a schedule
-//function to show all schedules
-//function to add both child and schedules together
-//function to delete and a button
-//function to add new records
-//function to clear the modal
-
-
-
-function getAllSchedulesWithChild(){
-    fetch (`${BASE_URL}/schedules`)
-    .then((resp) => {
-        return resp.json();
-    })
-    .then((schedules) => {
-        masterCount = schedules.length; 
-        // console.log(schedules[0].child_id);
-        displayMasterSchedules(schedules);
-    })
-    .catch((err) => {
-        console.log("Error:", err);
-    });
-
+function deleteEntry(e){
+    let btnNumber = parseInt(e.target.id);
+    deleteSchedule(btnNumber);
+ }
 }
+
+function displayMasterSchedules(masterSchedule){
+    for (let i = 0; i < scheduleCount; i++){ 
+        let id = masterSchedule[i].child_id;
+      //   console.log(id);
+      let schedule_id = masterSchedule[i].id;
+      let weekday = masterSchedule[i].weekday;
+      let date = masterSchedule[i].date;
+      let subject = masterSchedule[i].subject;
+      let content = masterSchedule[i].content;
+      buildChild(id);
+      let newPost = document.getElementById("schedules-container");
+      newPost.insertAdjacentHTML(
+        "beforeend",
+        `<br>
+          <div class="ui segment">
+              <h4 class="ui horizontal divider header">
+              <i class="child icon"></i>
+              Schedule
+              </h4>
+              <h5>Id: ${schedule_id}</h5>
+              <h5>Weekday: ${weekday}</h5>
+              <h5>Date: ${date}</h5>
+              <h5>Subject: ${subject}</h5>
+              <h5>Content: ${content}</h5>
+              <h4 class="ui horizontal divider header">
+               <i class="building icon"></i>
+               Child
+               </h4>
+               <div id="children-container${id}"></div>
+            <br />
+            <button id=${schedule_id} class="delBtn ui red right floated button" type="button">Discard</button>
+            <br/ >
+          <br />
+        </div>`
+      );
+    }
+    deleteButton();
+}
+
+function addScheduleRecords() {
+    let scheduleWeekday = document.querySelector("#input-weekday").value;
+    let scheduleDate = document.querySelector("#input-date").value;
+    let scheduleSubject = document.querySelector("#input-subject").value;
+    let scheduleContent = document.querySelector("#input-content").value;
+  
+    let listbox = document.getElementById("childList");
+    for (let c = 0; c < listbox.length; c++) {
+      if (listbox[c].selected) {
+        scheduleClass.saveChildSchedule(
+          listbox[c].value,
+          scheduleWeekday,
+          scheduleDate,
+          scheduleSubject,
+          scheduleContent
+        );
+      }
+    }
+    alert(`${scheduleWeekday} Schedule saved!`);
+  }
+  
+
 
 function buildChild(cid){
     fetch (`${BASE_URL}/children/${cid}`) 
@@ -85,58 +142,8 @@ function buildChild(cid){
     }) 
 }
 
-// basic delete functionality - COME BACK TO THIS
-function deleteButton(){
-    let btns = document.querySelectorAll(".delBtn");
-    for (let i = 0; i < btns.length; i++){
-        btns[i].addEventListener("click", deleteEntry);
-  }
 
-function deleteEntry(e){
-    let btnNumber = parseInt(e.target.id);
-    deleteSchedule(btnNumber);
-}
-}
 
-function displayMasterSchedules(masterSchedule){
-  for (let i = 0; i <masterCount; i++){ //line 80 min 48:54
-      let id = masterSchedule[i].child_id;
-    //   console.log(id);
-      let schedule_id = masterSchedule[i].id;
-      let weekday = masterSchedule[i].weekday;
-      let date = masterSchedule[i].date;
-      let subject = masterSchedule[i].subject;
-      let content = masterSchedule[i].content;
-      buildChild(id);
-      let newPost = document.getElementById("schedules-container");
-      newPost.insertAdjacentHTML(
-        "beforeend",
-        `<br>
-        <div class="ui segment">
-            <h4 class="ui horizontal divider header">
-            <i class="child icon"></i>
-            Schedule
-            </h4>
-            <h5>Id: ${schedule_id}</h5>
-            <h5>Weekday: ${weekday}</h5>
-            <h5>Date: ${date}</h5>
-            <h5>Subject: ${subject}</h5>
-            <h5>Content: ${content}</h5>
-            <h4 class="ui horizontal divider header">
-             <i class="building icon"></i>
-             Child
-             </h4>
-             <div id="children-container${id}"></div>
-          <br />
-          <button id=${id} class="delBtn ui red right floated button" type="button">Discard</button>
-          <br/ >
-        <br />
-      </div>`
-    );
-  }
-  deleteButton();
-
-}
 
 function getLastInsertedChild(result){
     alert(result);
