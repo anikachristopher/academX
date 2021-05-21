7e0aaf8e52babd733397b3eb462c652b1a200873
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-21 10:29:11 -0400

    create method to add the child record

--- academX_frontend/src/child.js
+++ academX_frontend/src/child.js
@@ -10,4 +10,34 @@ class Child{
     this.listElement = listElement;
     this.allChildURL = `${BASE_URL}/children`;
   }
+  
+  addChildRecords(childfirstName, childlastName, childGrade, childAge) {
+    fetch(this.allChildURL, {
+      method: "POST",
+      body: JSON.stringify({
+        first_name: childfirstName,
+        last_name: childlastName,
+        age: childAge,
+        grade: childGrade,
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    }).catch((err) => {
+      console.log("Error:", err);
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
